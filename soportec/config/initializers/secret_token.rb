1f962beef62704f5e14bb93d975ddec07479ae71
Ricardo Díaz <ricardotk002@gmail.com>
2013-05-31 20:36:45 -0500

    añadido proyecto soportec de ruby on rails

---
+++ soportec/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soportec::Application.config.secret_token = '60314ebe1de77434ea4355cf353758a2b690d2b5d0a865947a5e22fefab7c45fc490de08ad08a60d7351911438bad44aee48df4e75bc0f0be7eabe5eacfb478a'
