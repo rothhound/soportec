107ef7e171818737114398e5c4db21b23ea0e3ae
Gianfranco Montoya <gms122@gmail.com>
2013-06-30 19:13:45 -0500

    Registro de Horarios

--- soportec/app/controllers/schedules_controller.rb
+++ soportec/app/controllers/schedules_controller.rb
@@ -3,7 +3,9 @@ class SchedulesController < ApplicationController
   # GET /schedules.json
   def index
     @schedules = Schedule.all
-
+    @course = Course.all
+    @professor = Professor.all 
+    @day = Day.all
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @schedules }
@@ -14,7 +16,9 @@ class SchedulesController < ApplicationController
   # GET /schedules/1.json
   def show
     @schedule = Schedule.find(params[:id])
-
+    @course = Course.all
+    @professor = Professor.all
+    @day = Day.all
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @schedule }
@@ -25,30 +29,39 @@ class SchedulesController < ApplicationController
   # GET /schedules/new.json
   def new
     @schedule = Schedule.new
-
+    @course = Course.all
+    @professor = Professor.all
+    @day = Day.all
+    @current_method = "new"
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @schedule }
+      format.json { render :xml => @schedule }
     end
   end
 
   # GET /schedules/1/edit
   def edit
     @schedule = Schedule.find(params[:id])
+    @course = Course.all
+    @professor = Professor.all
+    @day = Day.all
+    @current_method = "update"
   end
 
   # POST /schedules
   # POST /schedules.json
   def create
     @schedule = Schedule.new(params[:schedule])
-
+    @course = Course.all
+    @professor = Professor.all
+    @day = Day.all
     respond_to do |format|
       if @schedule.save
         format.html { redirect_to @schedule, notice: 'Schedule was successfully created.' }
-        format.json { render json: @schedule, status: :created, location: @schedule }
+        format.json { render :xml => @schedule, status: :created, location: @schedule }
       else
         format.html { render action: "new" }
-        format.json { render json: @schedule.errors, status: :unprocessable_entity }
+        format.json { render :xml => @schedule.errors, status: :unprocessable_entity }
       end
     end
   end
