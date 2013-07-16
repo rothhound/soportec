0ee7211eda00b432ab6f62ed143eafe788fe06f7
Ricardo Diaz <ricardotk002@gmail.com>
2013-07-15 20:54:07 -0500

    Añadida validación para cursos

--- soportec/app/models/course.rb
+++ soportec/app/models/course.rb
@@ -1,8 +1,12 @@
 class Course < ActiveRecord::Base
-  attr_accessible :code, :name
+  attr_accessible :code, :name, :eap_id, :group_id
 
   has_and_belongs_to_many :professors
   belongs_to :eap
   belongs_to :group
   has_many :schedules
+
+  validates :code, :name, :eap_id, :group_id, presence: true
+  validates :code, numericality: {greater_than_or_equal_to: 201001}
+  validates :code, uniqueness: true
 end
