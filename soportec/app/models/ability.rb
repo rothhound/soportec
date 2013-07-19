b4c4eb62c5eff62e6a0498b22e9dba8d23734df4
Alexander Huaman <hark696@gmail.com>
2013-07-19 12:19:56 -0500

    Creaacion eliminacion eidicion de usuarios - Secretaria
    Ya edite los permisos para q la secre cree, elimine y edite usuarios.
    Claro todo limitado a q no puede tocar a un admin, y si crea no puede
    ser admin. Si edita tampoco.

--- soportec/app/models/ability.rb
+++ soportec/app/models/ability.rb
@@ -6,16 +6,21 @@ class Ability
 
     if user.role_id == 1 #administrador
         can :manage, :all
-        #can :assign_roles , User
+        cannot :assign_roles2 , User
     elsif user.role_id == 2 #secretaria
         can [:create,:destroy,:update], [Computer,Course,Laboratory,Professor,Schedule,Software]
-        can :editar , User, :id => user.id
-        can :update , User, :id => user.id
+        can [:update,:editar],User
         can :read , [Computer,Course,Laboratory,Professor,Schedule,Software,User]
         can :asignacion, Laboratory
         can :dynamic, Schedule
+        can :assign_roles2, User
+        can :create, User
+        can :destroy, User
 
-        cannot [:create, :destroy ,:assign_roles] , User
+        cannot :editar , User, :role_id => 1 
+        cannot :update , User, :role_id => 1 
+        cannot :destroy , User, :role_id => 1 
+        #cannot [:create, :destroy] , User
         cannot :manage, [Category,Eap,Group]
 
     elsif user.role_id == 3 #bolsista
@@ -23,7 +28,6 @@ class Ability
         can :update , User, :id => user.id
         can :read, [Course,Laboratory,Professor,Schedule,Software]
         
-        #cannot :manage , 
         cannot [:create,:destroy,:update], [Category,Computer,Course,Eap,Group,Laboratory,Professor,Schedule,Software]
         cannot :asignar, Laboratory
         #cannot [:read,,:assign_roles] , User
